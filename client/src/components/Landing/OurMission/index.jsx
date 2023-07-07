121f50cb78874b30fe11c97b99015b61cfb0b4c7
farah2003 <farah.shaqoura@example.com>
2023-07-07 21:17:41 +0300

    our mission

--- client/src/components/Landing/OurMission/index.jsx
+++ client/src/components/Landing/OurMission/index.jsx
@@ -14,9 +14,13 @@ const OurMission = () => (
       <div className="our-mission-description-container">
         <Paragraph className="title">Our Mission</Paragraph>
         <Paragraph className="our-mission-description">
-          Our mission is to help reach people in need and support them in
-          providing a better life, be hope for a hungry child or a father who
-          cannot educate his children, be hope for those in need.
+          provide assistance and support to individuals in need, improving their quality
+          of life and offering a beacon of hope. We aim to alleviate hunger among children
+          by providing nourishment and ensuring access to nutritious meals.
+          we empower fathers to provide education for their children, recognizing the
+          transformative power of education in breaking the cycle of poverty. Through our
+          efforts, we strive to be a source of hope for those facing hardships, inspiring
+          them to overcome challenges and pursue a better life.
         </Paragraph>
       </div>
     </section>
